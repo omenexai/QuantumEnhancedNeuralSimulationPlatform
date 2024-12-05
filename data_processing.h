8b6916651ed622a9f1fda6d818a42a497dffa724
omenex <imacalmman@gmail.com>
2024-12-05 08:44:36 +0100

    Create data_processing.h

---
+++ data_processing.h
@@ -0,0 +1,9 @@
+#ifndef DATA_PROCESSING_H
+#define DATA_PROCESSING_H
+
+// Function prototypes for data processing
+void preprocess_data(double *raw_data, double *processed_data);
+void normalize_data(double *data);
+void visualize_data(double *data);
+
+#endif // DATA_PROCESSING_H
