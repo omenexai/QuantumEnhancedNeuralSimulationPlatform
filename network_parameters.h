8b969ed2b6d7ce65164372f58f5545c28f12462e
omenex <imacalmman@gmail.com>
2024-12-05 08:41:38 +0100

    network_parameters.h

---
+++ network_parameters.h
@@ -0,0 +1,9 @@
+#ifndef NETWORK_PARAMETERS_H
+#define NETWORK_PARAMETERS_H
+
+// Function prototypes for network parameters management
+void set_network_size(int x, int y, int z);
+void set_activation_function(int activation_type);
+void set_coupling_constants(double J1, double J2, double J3);
+
+#endif // NETWORK_PARAMETERS_H
