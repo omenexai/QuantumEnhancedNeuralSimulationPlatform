978d7cee30fc03a5052fe89afaab58c952adb86e
omenex <imacalmman@gmail.com>
2024-12-05 08:42:00 +0100

    Create multi_scale_model.h

---
+++ multi_scale_model.h
@@ -0,0 +1,9 @@
+#ifndef MULTI_SCALE_MODEL_H
+#define MULTI_SCALE_MODEL_H
+
+// Function prototypes for multi-scale model management
+void initialize_multi_scale_model(int network_size);
+void run_multi_scale_simulation();
+void adjust_scale_parameters(double *params);
+
+#endif // MULTI_SCALE_MODEL_H
